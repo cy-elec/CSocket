fa35eb67f72fa2782a1e86c8c4719f089511d170
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-11-11 15:20:24 +0100

    adding compiled examples

--- exmpl/compile_all.sh
+++ exmpl/compile_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gcc -o simpleClient.o simpleClient.c -l:libcsocket.a -L../bin -I../bin
-gcc -o simpleServer.o simpleServer.c -l:libcsocket.a -L../bin -I../bin
-gcc -o multiServer.o multiServer.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleClient.o simpleClient.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleServer.o simpleServer.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/multiServer.o multiServer.c -l:libcsocket.a -L../bin -I../bin
 
-gcc -o simpleClient6.o simpleClient6.c -l:libcsocket.a -L../bin -I../bin
-gcc -o simpleServer6.o simpleServer6.c -l:libcsocket.a -L../bin -I../bin
-gcc -o multiServer6.o multiServer6.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleClient6.o simpleClient6.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleServer6.o simpleServer6.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/multiServer6.o multiServer6.c -l:libcsocket.a -L../bin -I../bin
--- exmpl/compile_allWin.sh
+++ exmpl/compile_allWin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gcc.exe -o simpleClient.exe simpleClient.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o simpleServer.exe simpleServer.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o multiServer.exe multiServer.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleClient.exe simpleClient.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleServer.exe simpleServer.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/multiServer.exe multiServer.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
 
-gcc.exe -o simpleClient6.exe simpleClient6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o simpleServer6.exe simpleServer6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o multiServer6.exe multiServer6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleClient6.exe simpleClient6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleServer6.exe simpleServer6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/multiServer6.exe multiServer6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
