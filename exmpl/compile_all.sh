ea831ea5a55b081ec350476b272ff06a092adb7b
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-11-28 18:16:00 +0100

    static link

--- exmpl/compile_all.sh
+++ exmpl/compile_all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-gcc -o bin/simpleClient.o simpleClient.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleServer.o simpleServer.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/multiServer.o multiServer.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleTransmitter.o simpleTransmitter.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/multiTransmitter.o multiTransmitter.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleReceiver.o simpleReceiver.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleStation1.o simpleStation1.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleStation2.o simpleStation2.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleStation61.o simpleStation61.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleStation62.o simpleStation62.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleClient.o simpleClient.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleServer.o simpleServer.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/multiServer.o multiServer.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleTransmitter.o simpleTransmitter.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/multiTransmitter.o multiTransmitter.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleReceiver.o simpleReceiver.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleStation1.o simpleStation1.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleStation2.o simpleStation2.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleStation61.o simpleStation61.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleStation62.o simpleStation62.c -static -l:libcsocket.a -L../bin -I../bin
 
-gcc -o bin/simpleClient6.o simpleClient6.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/simpleServer6.o simpleServer6.c -l:libcsocket.a -L../bin -I../bin
-gcc -o bin/multiServer6.o multiServer6.c -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleClient6.o simpleClient6.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/simpleServer6.o simpleServer6.c -static -l:libcsocket.a -L../bin -I../bin
+gcc -o bin/multiServer6.o multiServer6.c -static -l:libcsocket.a -L../bin -I../bin
 
 strip -s bin/*.o
--- exmpl/compile_allWin.sh
+++ exmpl/compile_allWin.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-gcc.exe -o bin/simpleClient.exe simpleClient.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleServer.exe simpleServer.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/multiServer.exe multiServer.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleTransmitter.exe simpleTransmitter.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/multiTransmitter.exe multiTransmitter.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleReceiver.exe simpleReceiver.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleStation1.exe simpleStation1.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleStation2.exe simpleStation2.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleStation61.exe simpleStation61.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleStation62.exe simpleStation62.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleClient.exe simpleClient.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleServer.exe simpleServer.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/multiServer.exe multiServer.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleTransmitter.exe simpleTransmitter.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/multiTransmitter.exe multiTransmitter.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleReceiver.exe simpleReceiver.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleStation1.exe simpleStation1.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleStation2.exe simpleStation2.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleStation61.exe simpleStation61.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleStation62.exe simpleStation62.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
 
-gcc.exe -o bin/simpleClient6.exe simpleClient6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/simpleServer6.exe simpleServer6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
-gcc.exe -o bin/multiServer6.exe multiServer6.c -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleClient6.exe simpleClient6.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/simpleServer6.exe simpleServer6.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
+gcc.exe -o bin/multiServer6.exe multiServer6.c -static -l:libcsocket.lib -lws2_32 -L../bin -I../bin
 
 strip.exe -s bin/*.exe
